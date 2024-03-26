49de543801a34f0f803f766e25db1bb91eec3b0f
elycuaton <115386914+elycuaton@users.noreply.github.com>
2024-03-26 14:51:01 -0600

    Button Functionality

--- CarMaintenanceApp/app/src/main/java/com/example/carmaintenanceapp/HomePage.java
+++ CarMaintenanceApp/app/src/main/java/com/example/carmaintenanceapp/HomePage.java
@@ -2,6 +2,10 @@ package com.example.carmaintenanceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import com.google.android.material.floatingactionbutton.FloatingActionButton; // For the FloatingActionButton
+
 
 
 public class HomePage extends AppCompatActivity {
@@ -10,6 +14,15 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addcar);
+
+        FloatingActionButton fabAddCar = findViewById(R.id.fabAddCar);
+        fabAddCar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomePage.this, Information.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
--- CarMaintenanceApp/app/src/main/java/com/example/carmaintenanceapp/SignUp.java
+++ CarMaintenanceApp/app/src/main/java/com/example/carmaintenanceapp/SignUp.java
@@ -1,8 +1,11 @@
 package com.example.carmaintenanceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
 
 public class SignUp extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        // Find the button by its ID
+        Button buttonSignUp = findViewById(R.id.btnSignUp);
+
+        // Set an OnClickListener for the button
+        buttonSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent to start the HomePage activity
+                Intent intent = new Intent(SignUp.this, HomePage.class);
+                startActivity(intent);  // Start the HomePage activity
+            }
+        });
     }
 }
