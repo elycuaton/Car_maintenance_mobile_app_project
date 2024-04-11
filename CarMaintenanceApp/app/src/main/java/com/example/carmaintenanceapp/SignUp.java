3413e64f4d6a4d5c403d435add0d2da3bad54a8c
jenitvilla <126704474+jenitvilla@users.noreply.github.com>
2024-04-11 00:22:28 -0600

    added sign up functionalities

--- CarMaintenanceApp/app/src/main/java/com/example/carmaintenanceapp/SignUp.java
+++ CarMaintenanceApp/app/src/main/java/com/example/carmaintenanceapp/SignUp.java
@@ -1,13 +1,27 @@
 package com.example.carmaintenanceapp;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.content.Intent;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SignUp extends AppCompatActivity {
 
+    EditText editTextName, editTextEmail, editTextPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,10 +29,64 @@ public class SignUp extends AppCompatActivity {
 
         Button buttonSignUp = findViewById(R.id.btnSignUp);
 
-        buttonSignUp.setOnClickListener(v -> {
+        editTextName = findViewById(R.id.editTextName);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPassword = findViewById(R.id.editTextPassword);
 
-            Intent intent = new Intent(SignUp.this, HomePage.class);
-            startActivity(intent);
+        buttonSignUp.setOnClickListener(v -> {
+            if(editTextPassword.getText().toString().isBlank()){
+                Toast.makeText(this, "Please fill out password.", Toast.LENGTH_SHORT).show();
+            }
+            else if(editTextName.getText().toString().isBlank()){
+                Toast.makeText(this, "Please fill out name.", Toast.LENGTH_SHORT).show();
+            }
+            else if(editTextEmail.getText().toString().isBlank()
+                    || !editTextEmail.getText().toString().contains("@")
+                    || !editTextEmail.getText().toString().contains(".")){
+                Toast.makeText(this, "Please enter a valid email.", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                sendPostApi();
+            }
+
         });
+    }
+    private void sendPostApi(){
+        RequestQueue requestQueue = Volley.newRequestQueue(SignUp.this);
+
+        String url = "http://10.0.2.2:5000/postUser";
+        StringRequest postRequest = new StringRequest(Request.Method.POST, url,
+                response -> {
+                    // Handle the response
+                    Toast.makeText(this, "Sign up successful. Please sign in to your account.", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(SignUp.this, MainActivity.class);
+                    startActivity(intent);
+                },
+                error -> {
+                    // Handle the error
+                    Log.d(TAG, "onCreate: "+ error.getMessage());
+                    Toast.makeText(this, "An error has occurred. Please try again.", Toast.LENGTH_SHORT).show();
+                }) {
+            @Override
+            public byte[] getBody() {
+                JSONObject body = new JSONObject();
+
+                try {
+                    body.put("name", editTextName.getText().toString());
+                    body.put("password", editTextPassword.getText().toString());
+                    body.put("email", editTextEmail.getText().toString());
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+                return body.toString().getBytes();
+            }
+
+            @Override
+            public String getBodyContentType() {
+                return "application/json";
+            }
+        };
+
+        requestQueue.add(postRequest);
     }
 }
